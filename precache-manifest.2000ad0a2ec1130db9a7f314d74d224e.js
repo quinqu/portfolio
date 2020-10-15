5d7f0efa50df485e90122da1ab3d6511d0ee16a6
quinqu <quinruby0@gmail.com>
2020-10-15 08:02:41 -0700

    Updates

---
+++ precache-manifest.2000ad0a2ec1130db9a7f314d74d224e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512f2aa23e640f63b30cf83ee2397e1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "593b58becdf45208d2b4",
+    "url": "/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "5255b93f2d5d42784b87",
+    "url": "/static/css/main.8f2dc061.chunk.css"
+  },
+  {
+    "revision": "593b58becdf45208d2b4",
+    "url": "/static/js/2.9863371e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9863371e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5255b93f2d5d42784b87",
+    "url": "/static/js/main.82e0348c.chunk.js"
+  },
+  {
+    "revision": "ce21c37ec8e492fe829d",
+    "url": "/static/js/runtime-main.8ee00f4e.js"
+  },
+  {
+    "revision": "1e153451f18bcbd335668dabf0b42373",
+    "url": "/static/media/echo.1e153451.jpg"
+  }
+]);
