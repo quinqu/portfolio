917dd6bbf388ce2a41893d226e4ece81260e8293
quinqu <quinruby0@gmail.com>
2020-10-15 12:28:10 -0700

    homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -27,7 +27,8 @@ class HomePage extends React.Component {
   render() {
     let currentItem = items[this.state.textIdx % items.length];
     return (
-      <JumboTron className="bg-transparent jumbotron-fluid p-0">  
+      <div>
+      <JumboTron className="bg-transparent jumbotron-fluid p-0 mb-0">  
         <Container fluid={true}> 
           <Row className="justify-content-center py-5"> 
             <Col md={8} sm={12}> 
@@ -38,8 +39,24 @@ class HomePage extends React.Component {
           </Row>
         </Container>
       </JumboTron>
-  );
-}
+      <img className="card-style-image-hp" src={"https://cdn.costumewall.com/wp-content/uploads/2019/08/jane-lane.jpg"} alt="avatar"/>
+
+        <div className="about">
+        <p className="justify-content-center"> Hi, my name is Jane. I am an entry level 
+          engineer with a passion for learning and building software. I am a musician who lives and 
+          breathes music. </p> 
+          
+          <p> I am currently interning 
+          at Gravitational as a Systems Software Engineer and I’m a full-stack 
+          development student at Ada Developers Academy, a highly immersive and 
+          competitive software engineering school in Seattle. 
+          </p>
+
+          <p> I currently reside in Seattle, but will eventually follow my heart and experience different places. </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
